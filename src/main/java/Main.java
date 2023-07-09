f98aeb0e3e0075bf52c2bf30b143676596d79adc
Anastasia <grraf.est@gmail.com>
2023-07-09 13:38:51 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
+
+        post.name = "Иван";
+        post.passport = "444 № 44444444";
+        post.patronymic = "Иванович";
+        post.phone = "+7 (999)-999-99-99";
+        post.surname = "Иванов";
+        post.subscription = "tru";
+
+        System.out.println(post.birthday);
+        System.out.println(post.birthday.day);
+        System.out.println(post.birthday.month);
+        System.out.println(post.passport);
+        System.out.println(post.patronymic);
+        System.out.println(post.phone);
+        System.out.println(post.surname);
+        System.out.println(post.subscription);
+
+    }
+}
